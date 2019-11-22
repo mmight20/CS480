0c42ff99cbd6bfeae7ae9e81c8849a5afba445aa
mmight20 <45834764+mmight20@users.noreply.github.com>
2019-11-21 22:37:20 -0500

    Add files via upload

---
+++ QuotableContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using quotable.api.models;
+
+/// <summary>
+///  The database context that provides access to document and other data.
+/// </summary>
+public class QuotableContext: DbContext
+{
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="options"></param>
+	public QuotableContext(DbContextOptions options): base(options)
+	{
+
+        public DbSet<Quote> Quote { get; set; }
+	}
+}
